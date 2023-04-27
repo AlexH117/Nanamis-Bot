3cb7adceab297139754f544d0bcd2a3249ad64f2
AlexH117 <56143825+AlexH117@users.noreply.github.com>
2023-04-27 12:59:41 -0600

    Ningun cambio
    Solo se puede llamar con un comando en especifico

--- main.js
+++ main.js
@@ -34,6 +34,23 @@ client.on('messageCreate', async (message) => {
 });
 
 async function generateResponse(prompt) {
+  try {
+    const response = await openai.complete({
+      engine: 'davinci-5',
+      prompt: prompt,
+      maxTokens: 1024,
+      n: 1,
+      stop: ['\n']
+    });
+    return response.choices[0].text.trim();
+  } catch (error) {
+    console.error('Error generating response:', error);
+    return 'Sorry, I am unable to generate a response at this time.';
+  }
+}
+
+
+async function generateResponse(prompt) {
     try {
         const response = await openai.createCompletion({
             model: 'text-davinci-002',
