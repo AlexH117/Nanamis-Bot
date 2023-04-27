66a125e5d053146ec3e77d0f344605c68592162f
AlexH117 <56143825+AlexH117@users.noreply.github.com>
2023-04-27 11:42:33 -0600

    Nanami funciona (a medias)
    Nanami funciona en el main.js (intentare actualizarlo a la ultima version de gpt-3 o gpt-4, pero se desconecta mucho (supongo que subiendolo a un host se reconectará automaticamente) y a veces contesta estupideces jsjsjs

--- index.js
+++ index.js
@@ -1,106 +1,56 @@
-require('dotenv').config();
-const {
-    Client,
-    GatewayIntentBits,
-    Partials
-} = require('discord.js');
-const {
-    Configuration,
-    OpenAIApi
-} = require('openai');
+const { Client, GatewayIntentBits, Partials, Message } = require("discord.js");
+const { token, prefix, openaiApiKey } = require("./config.json");
+const { ChatBot } = require("discord-chatbot");
+const openai = require("openai-api");
+
 const client = new Client({
-    partials: [Partials.Channel, Partials.Message, Partials.GuildMember],
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.MessageContent, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildVoiceStates]
+  intents: [
+    GatewayIntentBits.AutoModerationConfiguration,
+    GatewayIntentBits.AutoModerationExecution,
+    GatewayIntentBits.DirectMessageReactions,
+    GatewayIntentBits.DirectMessageTyping,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.GuildIntegrations,
+    GatewayIntentBits.GuildInvites,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildMessageTyping,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildModeration,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildScheduledEvents,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildWebhooks,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent,
+  ],
+
+  partials: [
+    Partials.Channel,
+    Partials.GuildMember,
+    Partials.GuildScheduledEvent,
+    Partials.Message,
+    Partials.Reaction,
+    Partials.ThreadMember,
+    Partials.User,
+  ]
 });
-let data = {
-    GPT_API_KEY: process.env.GPT_API_KEY,
-    DISCORD_BOT_TOKEN: process.env.DISCORD_BOT_TOKEN
-};
 
-const configuration = new Configuration({
-    apiKey: data.GPT_API_KEY
+const chatbot = new ChatBot({ name: "Nanami", openaiApiKey: openaiApiKey });
+// generateResponse(message.content, openaiAPI)
+const openaiAPI = new openai(openaiApiKey);
+
+//No borrar________________________________________
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
 });
-const openai = new OpenAIApi(configuration);
+//_________________________________________________
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('messageCreate', async (message) => {
-    if (message.author.bot) return;
-
-    const response = await generateResponse(message.content);
-    message.channel.send(response);
+client.on("messageCreate", async (message) => {
+  if (!message.content.startsWith("!") && !message.author.bot) {
+    const response = await chatbot.generateResponse(message.content, openaiAPI);
+    message.reply(response);
+  }
 });
 
-async function generateResponse(prompt) {
-    try {
-        const response = await openai.createCompletion({
-            model: 'text-davinci-002',
-            prompt,
-            max_tokens: 1024,
-            n: 1
-        });
-        
-        return response.data.choices[0].text.trim();
-    } catch (error) {
-        console.error('Error generating response:', error.response ? error.response.data : error);
-        return 'Sorry, I am unable to generate a response at this time.';
-    }
-}
-
-client.login(data.DISCORD_BOT_TOKEN);
-
-// const { Client, GatewayIntentBits, Partials, Message } = require('discord.js');
-// const { token, prefix, openaiApiKey } = require("./config.json");
-// const { ChatBot } = require('discord-chatbot');
-// const openai = require('openai-api')
-
-// const client = new Client({
-//   intents: [
-//     GatewayIntentBits.AutoModerationConfiguration,
-//     GatewayIntentBits.AutoModerationExecution,
-//     GatewayIntentBits.DirectMessageReactions,
-//     GatewayIntentBits.DirectMessageTyping,
-//     GatewayIntentBits.DirectMessages,
-//     GatewayIntentBits.GuildIntegrations,
-//     GatewayIntentBits.GuildInvites,
-//     GatewayIntentBits.GuildMembers,
-//     GatewayIntentBits.GuildMessageReactions,
-//     GatewayIntentBits.GuildMessageTyping,
-//     GatewayIntentBits.GuildMessages,
-//     GatewayIntentBits.GuildModeration,
-//     GatewayIntentBits.GuildPresences,
-//     GatewayIntentBits.GuildScheduledEvents,
-//     GatewayIntentBits.GuildVoiceStates,
-//     GatewayIntentBits.GuildWebhooks,
-//     GatewayIntentBits.Guilds,
-//     GatewayIntentBits.MessageContent
-//   ],
-
-//   partials: [
-//     Partials.Channel,
-//     Partials.GuildMember,
-//     Partials.GuildScheduledEvent,
-//     Partials.Message,
-//     Partials.Reaction,
-//     Partials.ThreadMember,
-//     Partials.User
-//   ]
-// });
-
-// const chatbot = new ChatBot({ name: 'Nanami', openaiApiKey: openaiApiKey});
-// const openaiAPI = new openai(openaiApiKey);
-
-// client.on('ready', () => {
-//   console.log(`Logged in as ${client.user.tag}`);
-// });
-
-// client.on('messageCreate', async message => {
-//   if (!message.content.startsWith('!Nanami') && !message.author.bot) {
-//     const response = await chatbot.generateResponse(message.content, openaiAPI);
-//     message.reply(response);
-//   }
-// });
-
-// client.login(token);
+client.login(token);
